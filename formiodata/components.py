6b95b7b5152ae6c55daff7457759f664c662d80c
Bob Leers <bob@novacode.nl>
2021-02-28 10:46:26 +0100

    [IMP] Compatibility workaround: datetime.fromisoformat, not available in Python < 3.7

--- formiodata/components.py
+++ formiodata/components.py
@@ -224,12 +224,24 @@ class datetimeComponent(Component):
     def _fromisoformat(self, value):
         # Backport of Python 3.7 datetime.fromisoformat
         if hasattr(datetime, 'fromisoformat'):
-            # == Python 3.7
+            # Python >= 3.7
             return datetime.fromisoformat(value)
         else:
-            # < Python 3.7
-            fmt_str =  r"%Y-%m-%dT%H:%M:%S.%f" # replaces the fromisoformatm, not available in python 3.6
-            return datetime.datetime.strptime(value, fmt_str)
+            # Python < 3.7
+            # replaces the fromisoformat, not available in Python < 3.7
+            #
+            # XXX following:
+            # - Raises: '2021-02-25T00:00:00+01:00' does not match format '%Y-%m-%dT%H:%M%z'
+            # - Due to %z not obtaing the colon in '+1:00' (tz offset)
+            # - More info: https://stackoverflow.com/questions/54268458/datetime-strptime-issue-with-a-timezone-offset-with-colons
+            # fmt_str =  r"%Y-%m-%dT%H:%M:%S%z"
+            # return datetime.strptime(value, fmt_str)
+            #
+            # REQUIREMENT (TODO document, setup dependency or try/except raise exception)
+            # - pip install dateutil
+            # - https://dateutil.readthedocs.io/
+            from dateutil.parser import parse
+            return parse(value)
 
     @property
     def value(self):
@@ -306,7 +318,6 @@ class datetimeComponent(Component):
     def to_datetime(self):
         if not self.raw_value:
             return None
-        component = self.builder.components.get(self.key)
         dt = self._fromisoformat(self.raw_value)
         return dt
 
@@ -354,10 +365,9 @@ class panelComponent(Component):
 
     @property
     def title(self):
-        component = self.builder.components.get(self.key)
-        title = component.raw.get('title')
+        title = self.raw.get('title')
         if not title:
-            title = component.raw.get('label')
+            title = self.raw.get('label')
 
         if self.i18n.get(self.language):
             return self.i18n[self.language].get(title, title)
