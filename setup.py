22242d7d101071a9798ca245f36c8c7f7e07e43c
Bob Leers <bob@novacode.nl>
2021-05-16 14:05:12 +0200

    Bump version 0.3.4 (Fix dayComponent and unittests)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = find_packages(exclude=['tests*'])
 
 setup(
     name='formio-data',
-    version='0.3.3',
+    version='0.3.4',
     description='Form.io JSON-data API',
     url='https://github.com/novacode-nl/python-formio-data',
     author='Bob Leers',
