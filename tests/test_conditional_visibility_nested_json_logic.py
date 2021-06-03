c90be7f65c510c170b659d44209612eeaca2c195
Peter Bex <peter@more-magic.net>
2021-06-03 15:31:58 +0200

    Fix JSON conditional logic to work properly on global data (#14)
    Before, conditional logic would only see the current row's data.  To
    extend this, we need to pass on the full original data map to each
    nested component.
    
    Working on this exposed a potential issue: if "value" is used in the
    form as a key, it will be clobbered by the current component in the
    hierarchy.  This means conditional logic won't work properly if it
    depends on this "value" element.
    
    The proper fix would be to have the form property encode just the raw
    JSON form and have another property for the parsed and Python-encoded
    value.

--- tests/test_conditional_visibility_nested_json_logic.py
+++ tests/test_conditional_visibility_nested_json_logic.py
@@ -16,6 +16,8 @@ class ConditionalVisibilityNestedJsonLogicTestCase(unittest.TestCase):
         self.builder_json = readfile('data', 'test_conditional_visibility_nested_json_logic_builder.json')
         self.hide_secret_form_json = readfile('data', 'test_conditional_visibility_nested_json_logic_hide_secret.json')
         self.show_secret_form_json = readfile('data', 'test_conditional_visibility_nested_json_logic_show_secret.json')
+        self.hide_global_secret_only_form_json = readfile('data', 'test_conditional_visibility_nested_json_logic_hide_global_secret_only.json')
+        self.show_global_secret_only_form_json = readfile('data', 'test_conditional_visibility_nested_json_logic_show_global_secret_only.json')
 
 
     def test_conditionally_shown_top_level_form_elements_have_default_state_in_builder(self):
@@ -74,7 +76,7 @@ class ConditionalVisibilityNestedJsonLogicTestCase(unittest.TestCase):
         self.assertTrue(show_secret_form.input_components['secret2'].conditionally_visible)
 
 
-    def test_conditionally_shown_form_elements_in_data_grid_toggle_on_condition_met(self):
+    def test_conditionally_shown_form_elements_in_data_grid_toggle_on_local_row_condition_met(self):
         builder = Builder(self.builder_json)
 
         hide_secret_form = Form(self.hide_secret_form_json, builder)
@@ -84,11 +86,13 @@ class ConditionalVisibilityNestedJsonLogicTestCase(unittest.TestCase):
         self.assertTrue(hide_secret_first_row.input_components['username3'].conditionally_visible)
         self.assertTrue(hide_secret_first_row.input_components['password3'].conditionally_visible)
         self.assertFalse(hide_secret_first_row.input_components['secret3'].conditionally_visible)
+        self.assertFalse(hide_secret_first_row.input_components['globalSecret'].conditionally_visible)
 
         hide_secret_second_row = hide_secret_datagrid.rows[1]
         self.assertTrue(hide_secret_second_row.input_components['username3'].conditionally_visible)
         self.assertTrue(hide_secret_second_row.input_components['password3'].conditionally_visible)
         self.assertFalse(hide_secret_second_row.input_components['secret3'].conditionally_visible)
+        self.assertFalse(hide_secret_second_row.input_components['globalSecret'].conditionally_visible)
 
         show_secret_form = Form(self.show_secret_form_json, builder)
         show_secret_datagrid = show_secret_form.input_components['dataGrid']
@@ -97,11 +101,47 @@ class ConditionalVisibilityNestedJsonLogicTestCase(unittest.TestCase):
         self.assertTrue(show_secret_first_row.input_components['username3'].conditionally_visible)
         self.assertTrue(show_secret_first_row.input_components['password3'].conditionally_visible)
         self.assertTrue(show_secret_first_row.input_components['secret3'].conditionally_visible)
+        self.assertTrue(show_secret_first_row.input_components['globalSecret'].conditionally_visible)
 
         show_secret_second_row = show_secret_datagrid.rows[0]
         self.assertTrue(show_secret_second_row.input_components['username3'].conditionally_visible)
         self.assertTrue(show_secret_second_row.input_components['password3'].conditionally_visible)
         self.assertTrue(show_secret_second_row.input_components['secret3'].conditionally_visible)
+        self.assertTrue(show_secret_second_row.input_components['globalSecret'].conditionally_visible)
+
+
+    def test_conditionally_shown_form_elements_in_data_grid_toggle_on_global_data_condition_met(self):
+        builder = Builder(self.builder_json)
+
+        show_global_secret_only_form = Form(self.show_global_secret_only_form_json, builder)
+        show_global_secret_only_datagrid = show_global_secret_only_form.input_components['dataGrid']
+
+        show_global_secret_only_first_row = show_global_secret_only_datagrid.rows[0]
+        self.assertTrue(show_global_secret_only_first_row.input_components['username3'].conditionally_visible)
+        self.assertTrue(show_global_secret_only_first_row.input_components['password3'].conditionally_visible)
+        self.assertFalse(show_global_secret_only_first_row.input_components['secret3'].conditionally_visible)
+        self.assertTrue(show_global_secret_only_first_row.input_components['globalSecret'].conditionally_visible)
+
+        show_global_secret_only_second_row = show_global_secret_only_datagrid.rows[1]
+        self.assertTrue(show_global_secret_only_second_row.input_components['username3'].conditionally_visible)
+        self.assertTrue(show_global_secret_only_second_row.input_components['password3'].conditionally_visible)
+        self.assertFalse(show_global_secret_only_second_row.input_components['secret3'].conditionally_visible)
+        self.assertTrue(show_global_secret_only_second_row.input_components['globalSecret'].conditionally_visible)
+
+        hide_global_secret_only_form = Form(self.hide_global_secret_only_form_json, builder)
+        hide_global_secret_only_datagrid = hide_global_secret_only_form.input_components['dataGrid']
+
+        hide_global_secret_only_first_row = hide_global_secret_only_datagrid.rows[0]
+        self.assertTrue(hide_global_secret_only_first_row.input_components['username3'].conditionally_visible)
+        self.assertTrue(hide_global_secret_only_first_row.input_components['password3'].conditionally_visible)
+        self.assertTrue(hide_global_secret_only_first_row.input_components['secret3'].conditionally_visible)
+        self.assertFalse(hide_global_secret_only_first_row.input_components['globalSecret'].conditionally_visible)
+
+        hide_global_secret_only_second_row = hide_global_secret_only_datagrid.rows[0]
+        self.assertTrue(hide_global_secret_only_second_row.input_components['username3'].conditionally_visible)
+        self.assertTrue(hide_global_secret_only_second_row.input_components['password3'].conditionally_visible)
+        self.assertTrue(hide_global_secret_only_second_row.input_components['secret3'].conditionally_visible)
+        self.assertFalse(hide_global_secret_only_second_row.input_components['globalSecret'].conditionally_visible)
 
 
     def test_conditionally_shown_top_level_form_elements_do_not_render_when_hidden(self):
@@ -144,7 +184,7 @@ class ConditionalVisibilityNestedJsonLogicTestCase(unittest.TestCase):
         self.assertEqual('<p>Secret message</p>', show_secret_form.input_components['secret2'].html_component)
 
 
-    def test_conditionally_shown_form_elements_in_data_grid_do_not_render_when_hidden(self):
+    def test_conditionally_shown_form_elements_for_local_row_condition_in_data_grid_do_not_render_when_hidden(self):
         builder = Builder(self.builder_json)
 
         hide_secret_form = Form(self.hide_secret_form_json, builder)
@@ -152,16 +192,18 @@ class ConditionalVisibilityNestedJsonLogicTestCase(unittest.TestCase):
         hide_secret_datagrid = hide_secret_form.input_components['dataGrid']
 
         hide_secret_first_row = hide_secret_datagrid.rows[0]
-        self.assertEqual('<tr><td><table><tr><td><p>wrong</p></td><td><p>incorrect</p></td><td></td></tr></table></td></tr>', hide_secret_first_row.html_component)
+        self.assertEqual('<tr><td><table><tr><td><p>wrong</p></td><td><p>incorrect</p></td><td></td><td></td></tr></table></td></tr>', hide_secret_first_row.html_component)
         self.assertEqual('<p>wrong</p>', hide_secret_first_row.input_components['username3'].html_component)
         self.assertEqual('<p>incorrect</p>', hide_secret_first_row.input_components['password3'].html_component)
         self.assertEqual('', hide_secret_first_row.input_components['secret3'].html_component)
+        self.assertEqual('', hide_secret_first_row.input_components['globalSecret'].html_component)
 
         hide_secret_second_row = hide_secret_datagrid.rows[1]
-        self.assertEqual('<tr><td><table><tr><td><p>wrong</p></td><td><p>incorrect</p></td><td></td></tr></table></td></tr>', hide_secret_second_row.html_component)
+        self.assertEqual('<tr><td><table><tr><td><p>wrong</p></td><td><p>incorrect</p></td><td></td><td></td></tr></table></td></tr>', hide_secret_second_row.html_component)
         self.assertEqual('<p>wrong</p>', hide_secret_second_row.input_components['username3'].html_component)
         self.assertEqual('<p>incorrect</p>', hide_secret_second_row.input_components['password3'].html_component)
         self.assertEqual('', hide_secret_second_row.input_components['secret3'].html_component)
+        self.assertEqual('', hide_secret_second_row.input_components['globalSecret'].html_component)
 
         # Tying it all together
         self.assertEqual(
@@ -174,19 +216,73 @@ class ConditionalVisibilityNestedJsonLogicTestCase(unittest.TestCase):
         show_secret_datagrid = show_secret_form.input_components['dataGrid']
 
         show_secret_first_row = show_secret_datagrid.rows[0]
-        self.assertEqual('<tr><td><table><tr><td><p>user</p></td><td><p>secret</p></td><td><p>Secret message</p></td></tr></table></td></tr>', show_secret_first_row.html_component)
+        self.assertEqual('<tr><td><table><tr><td><p>user</p></td><td><p>secret</p></td><td><p>Secret message</p></td><td><p>Another secret message</p></td></tr></table></td></tr>', show_secret_first_row.html_component)
         self.assertEqual('<p>user</p>', show_secret_first_row.input_components['username3'].html_component)
         self.assertEqual('<p>secret</p>', show_secret_first_row.input_components['password3'].html_component)
         self.assertEqual('<p>Secret message</p>', show_secret_first_row.input_components['secret3'].html_component)
+        self.assertEqual('<p>Another secret message</p>', show_secret_first_row.input_components['globalSecret'].html_component)
 
         show_secret_second_row = show_secret_datagrid.rows[0]
-        self.assertEqual('<tr><td><table><tr><td><p>user</p></td><td><p>secret</p></td><td><p>Secret message</p></td></tr></table></td></tr>', show_secret_second_row.html_component)
+        self.assertEqual('<tr><td><table><tr><td><p>user</p></td><td><p>secret</p></td><td><p>Secret message</p></td><td><p>Another secret message</p></td></tr></table></td></tr>', show_secret_second_row.html_component)
         self.assertEqual('<p>user</p>', show_secret_second_row.input_components['username3'].html_component)
         self.assertEqual('<p>secret</p>', show_secret_second_row.input_components['password3'].html_component)
         self.assertEqual('<p>Secret message</p>', show_secret_second_row.input_components['secret3'].html_component)
+        self.assertEqual('<p>Another secret message</p>', show_secret_second_row.input_components['globalSecret'].html_component)
 
         # Tying it all together
         self.assertEqual(
             f'<table>{show_secret_first_row.html_component}{show_secret_second_row.html_component}</table>',
             show_secret_datagrid.html_component
+        )
+
+
+    def test_conditionally_shown_form_elements_for_global_data_condition_in_data_grid_do_not_render_when_hidden(self):
+        builder = Builder(self.builder_json)
+
+        show_global_secret_only_form = Form(self.show_global_secret_only_form_json, builder)
+        show_global_secret_only_form.render_components()
+        show_global_secret_only_datagrid = show_global_secret_only_form.input_components['dataGrid']
+
+        show_global_secret_only_first_row = show_global_secret_only_datagrid.rows[0]
+        self.assertEqual('<tr><td><table><tr><td><p>wrong</p></td><td><p>incorrect</p></td><td></td><td><p>Another secret message</p></td></tr></table></td></tr>', show_global_secret_only_first_row.html_component)
+        self.assertEqual('<p>wrong</p>', show_global_secret_only_first_row.input_components['username3'].html_component)
+        self.assertEqual('<p>incorrect</p>', show_global_secret_only_first_row.input_components['password3'].html_component)
+        self.assertEqual('', show_global_secret_only_first_row.input_components['secret3'].html_component)
+        self.assertEqual('<p>Another secret message</p>', show_global_secret_only_first_row.input_components['globalSecret'].html_component)
+
+        show_global_secret_only_second_row = show_global_secret_only_datagrid.rows[1]
+        self.assertEqual('<tr><td><table><tr><td><p>wrong</p></td><td><p>incorrect</p></td><td></td><td><p>Another secret message</p></td></tr></table></td></tr>', show_global_secret_only_second_row.html_component)
+        self.assertEqual('<p>wrong</p>', show_global_secret_only_second_row.input_components['username3'].html_component)
+        self.assertEqual('<p>incorrect</p>', show_global_secret_only_second_row.input_components['password3'].html_component)
+        self.assertEqual('', show_global_secret_only_second_row.input_components['secret3'].html_component)
+        self.assertEqual('<p>Another secret message</p>', show_global_secret_only_second_row.input_components['globalSecret'].html_component)
+
+        # Tying it all together
+        self.assertEqual(
+            f'<table>{show_global_secret_only_first_row.html_component}{show_global_secret_only_second_row.html_component}</table>',
+            show_global_secret_only_datagrid.html_component
+        )
+
+        hide_global_secret_only_form = Form(self.hide_global_secret_only_form_json, builder)
+        hide_global_secret_only_form.render_components()
+        hide_global_secret_only_datagrid = hide_global_secret_only_form.input_components['dataGrid']
+
+        hide_global_secret_only_first_row = hide_global_secret_only_datagrid.rows[0]
+        self.assertEqual('<tr><td><table><tr><td><p>user</p></td><td><p>secret</p></td><td><p>Secret message</p></td><td></td></tr></table></td></tr>', hide_global_secret_only_first_row.html_component)
+        self.assertEqual('<p>user</p>', hide_global_secret_only_first_row.input_components['username3'].html_component)
+        self.assertEqual('<p>secret</p>', hide_global_secret_only_first_row.input_components['password3'].html_component)
+        self.assertEqual('<p>Secret message</p>', hide_global_secret_only_first_row.input_components['secret3'].html_component)
+        self.assertEqual('', hide_global_secret_only_first_row.input_components['globalSecret'].html_component)
+
+        hide_global_secret_only_second_row = hide_global_secret_only_datagrid.rows[0]
+        self.assertEqual('<tr><td><table><tr><td><p>user</p></td><td><p>secret</p></td><td><p>Secret message</p></td><td></td></tr></table></td></tr>', hide_global_secret_only_second_row.html_component)
+        self.assertEqual('<p>user</p>', hide_global_secret_only_second_row.input_components['username3'].html_component)
+        self.assertEqual('<p>secret</p>', hide_global_secret_only_second_row.input_components['password3'].html_component)
+        self.assertEqual('<p>Secret message</p>', hide_global_secret_only_second_row.input_components['secret3'].html_component)
+        self.assertEqual('', hide_global_secret_only_second_row.input_components['globalSecret'].html_component)
+
+        # Tying it all together
+        self.assertEqual(
+            f'<table>{hide_global_secret_only_first_row.html_component}{hide_global_secret_only_second_row.html_component}</table>',
+            hide_global_secret_only_datagrid.html_component
         )
