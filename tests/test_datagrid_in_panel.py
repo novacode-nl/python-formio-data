bfbd2d6a14cde6c83444c7ae364494b0f7380677
Peter Bex <peter@more-magic.net>
2021-06-03 12:45:38 +0200

    Fix bug that caused subcomponents to be double-processed (#17)
    The code that's marked as "iffy" in load_data would be processing
    input components nested under other components inside panels (or other
    pure layout components), even if the loop above it had already
    processed them through the child component, causing a misparenting and
    double instantiation of input components to happen.
    
    As a hack, we skip processing the immediate subcomponents, as they are
    already processed by the upper loop.  Eventually we should get rid of
    the iffy second loop, but I'm not sure how to, yet.
    
    More importantly, this adds a regression test to make sure that when
    we do refactor this code, the bug won't reappear.

---
+++ tests/test_datagrid_in_panel.py
@@ -0,0 +1,45 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+import json
+import unittest
+
+from tests.utils import readfile
+from formiodata.builder import Builder
+from formiodata.form import Form
+from formiodata.components import textfieldComponent, passwordComponent
+
+
+# Regression test for #17
+class DatagridInPanelTestcase(unittest.TestCase):
+    def setUp(self):
+        super(DatagridInPanelTestcase, self).setUp()
+
+        self.builder_json = readfile('data', 'test_datagrid_in_panel_builder.json')
+        self.one_row_form_json = readfile('data', 'test_datagrid_in_panel_one_row_form.json')
+
+
+    def test_default_state_in_builder_has_one_row(self):
+        builder = Builder(self.builder_json)
+        self.assertEqual({'panel', 'submit'}, set(builder.components.keys()))
+
+        panel = builder.components['panel']
+        self.assertEqual({'dataGrid'}, set(panel.components.keys()))
+
+        datagrid = builder.components['panel'].components['dataGrid']
+        self.assertEqual({'textField'}, set(datagrid.components.keys()))
+        self.assertEqual(len(datagrid.rows), 0)
+
+
+    def test_form_with_one_row_has_the_one_row_created_by_submission(self):
+        builder = Builder(self.builder_json)
+
+        form = Form(self.one_row_form_json, builder)
+        self.assertEqual({'panel', 'submit'}, set(form.components.keys()))
+
+        panel = form.components['panel']
+        self.assertEqual({'dataGrid'}, set(panel.components.keys()))
+
+        datagrid = form.components['panel'].components['dataGrid']
+        self.assertEqual({'textField'}, set(datagrid.components.keys()))
+        self.assertEqual(len(datagrid.rows), 1)
