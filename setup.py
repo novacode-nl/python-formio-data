0f4c0fecbfe8bd267e559779fddc84cf29739465
Bob Leers <bob@novacode.nl>
2021-06-07 09:34:18 +0200

    Bump 0.3.11
    Changelog:
    - Fix JSON conditional logic to work properly on global data (#14)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = find_packages(exclude=['tests*'])
 
 setup(
     name='formio-data',
-    version='0.3.10',
+    version='0.3.11',
     description='Form.io JSON-data API',
     url='https://github.com/novacode-nl/python-formio-data',
     author='Bob Leers',
