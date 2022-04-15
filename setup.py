873bf32d8bd2e3f7a978baae005f2ed547831bca
Bob Leers <bob@novacode.nl>
2022-04-15 13:19:30 +0200

    Bump version 0.4.0
    Increased version 0.4.0 due to change in tableComponent
    rows (property) structure.
    
    Changes:
    - PR #23: Add a direnv configuration using Nixpkg
    - PR #26: Fix loading of tableComponents and correct unit tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = find_packages(exclude=['tests*'])
 
 setup(
     name='formio-data',
-    version='0.3.14',
+    version='0.3.15',
     description='formio.js JSON-data API',
     url='https://github.com/novacode-nl/python-formio-data',
     author='Bob Leers',
