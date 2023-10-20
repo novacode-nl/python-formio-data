90719b62042984eba7efdf0a9862d5e94bfd4a1d
bobslee <bob@novacode.nl>
2023-10-20 16:46:37 +0200

    Fix validation_errors (row level) for baseGridComponent
    Improve unittest concerning datagrid component with multiple valid and
    invalid rows.

--- tests/test_validation_error_simple.py
+++ tests/test_validation_error_simple.py
@@ -1,10 +1,19 @@
 # Copyright Nova Code (http://www.novacode.nl)
 # See LICENSE file for full licensing details.
 
+from tests.utils import readfile
 from test_component import ComponentTestCase
+
+from formiodata.form import Form
 
 
 class ValidationErrorSimpleTestCase(ComponentTestCase):
+
+    def setUp(self):
+        super(ValidationErrorSimpleTestCase, self).setUp()
+        self.form_validation_errors_json = readfile('data', 'test_example_form_validation_errors.json')
+        self.form_validation_errors = Form(self.form_validation_errors_json, self.builder)
+        self.form_validation_errors_i18n_nl = Form(self.form_validation_errors_json, self.builder_i18n_nl)
 
     def test_required_components_in_builder(self):
         firstName = self.builder.input_components['firstName']
@@ -17,7 +26,7 @@ class ValidationErrorSimpleTestCase(ComponentTestCase):
         self.assertFalse(email.required)
 
     def test_required_components_form_validation_errors(self):
-        errors = self.form_empty.validation_errors()
+        errors = self.form_validation_errors.validation_errors()
 
         self.assertEqual(
             errors['firstName']['required'],
@@ -31,9 +40,21 @@ class ValidationErrorSimpleTestCase(ComponentTestCase):
             errors['dataGrid'][0]['textField']['required'],
             'Text Field is required'
         )
+        self.assertEqual(
+            errors['dataGrid'][1],
+            {}
+        )
+        self.assertEqual(
+            errors['dataGrid'][2]['textField']['required'],
+            'Text Field is required'
+        )
+        self.assertEqual(
+            errors['dataGrid'][3],
+            {}
+        )
 
     def test_required_components_form_validation_errors_i18n_nl(self):
-        errors = self.form_empty_i18n_nl.validation_errors()
+        errors = self.form_validation_errors_i18n_nl.validation_errors()
 
         self.assertEqual(
             errors['firstName']['required'],
@@ -47,7 +68,19 @@ class ValidationErrorSimpleTestCase(ComponentTestCase):
             errors['dataGrid'][0]['textField']['required'],
             'Tekstveld is verplicht'
         )
+        self.assertEqual(
+            errors['dataGrid'][1],
+            {}
+        )
+        self.assertEqual(
+            errors['dataGrid'][2]['textField']['required'],
+            'Tekstveld is verplicht'
+        )
+        self.assertEqual(
+            errors['dataGrid'][3],
+            {}
+        )
 
     def test_not_required_components_form(self):
-        errors = self.form_empty_i18n_nl.validation_errors()
+        errors = self.form_validation_errors_i18n_nl.validation_errors()
         self.assertNotIn('email', errors)
